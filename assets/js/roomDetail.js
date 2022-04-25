2285090a26bdcaf7bf10355405c16e03ad95b446
tuanduong1209 <tuanduong19077@gmail.com>
2022-04-25 13:16:10 +0700

    tuan update

---
+++ assets/js/roomDetail.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('.roomDetailSlideMain').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+      });
+      $('.roomDetailSlideList').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true
+      });
+})
